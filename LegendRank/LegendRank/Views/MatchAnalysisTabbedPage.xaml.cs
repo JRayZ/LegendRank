f75c72cc8bd410ac757145ad0d68a575134053bc
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-10-14 19:00:55 -0400

    created Views and modified Controls

---
+++ LegendRank/LegendRank/Views/MatchAnalysisTabbedPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LolApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MatchAnalysisTabbedPage : TabbedPage
+    {
+        public MatchAnalysisTabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LegendRank/LegendRank/Views/MatchTotalPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LolApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MatchTotalPage : ContentPage
+    {
+        public MatchTotalPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
